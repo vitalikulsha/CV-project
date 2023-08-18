cce3cc3dfadb087090f07416f1a1eabcd6332c33
Vitali Kulsha <kulsha_85@mail.ru>
2023-08-18 15:16:44 +0300

    add router

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 /* class HTML Builder*/
 class HtmlBuilder {
-    constructor() {
-        this.nav = [
-            { 'link': '#', 'title': 'Главная' },
-            { 'link': 'pages/developer/index.html', 'title': 'Я разработчик' },
-            { 'link': '#', 'title': 'Я инженер' },
-            { 'link': '#', 'title': 'Контакты' },
-        ];
-    }
+    nav = [
+        { 'link': '#', 'title': 'Главная' },
+        { 'link': 'pages/developer/index.html', 'title': 'Я разработчик' },
+        { 'link': '#', 'title': 'Я инженер' },
+        { 'link': '#', 'title': 'Контакты' },
+    ];
+
+    constructor() { }
 
     getNav() {
         return this.nav;
@@ -54,5 +54,106 @@ class HtmlBuilder {
     }
 }
 
+class Router {
+    routes = [];
+    mode = null;
+    root = '/';
+
+    constructor(options) {
+        this.mode = window.history.pushState ? 'history' : 'hash';
+        if (options.mode) {
+            this.mode = options.mode;
+        }
+        if (options.root) {
+            this.root = options.root;
+        }
+    }
+
+    add = (path, cb) => {
+        this.routes.push({
+            path,
+            cb
+        })
+        return this;
+    }
+
+    remove = path => {
+        for (let i = 0; i < this.routes.length; i++) {
+            if (this.routes[i].path === path) {
+                this.routes.slice(i, 1);
+                return this;
+            }
+        }
+        return this;
+    }
+
+    flush = () => {
+        this.routes = [];
+        return this;
+    }
+
+    clearSlashes = path => path.toString()
+        .replace(/\/$/, '')
+        .replace(/^\//, '');
+
+    getFragment = () => {
+        let fragment = '';
+        if (this.mode === 'history') {
+            fragment = this.clearSlashes(decodeURI(window.location.pathname + window.location.search));
+            fragment = fragment.replace(/\?(.*)$/, '');
+            fragment = this.root !== '/' ? fragment.replace(this.root, '') : fragment;
+        } else {
+            const match = window.location.href.match(/#(.*)$/);
+            fragment = match ? match[1] : '';
+        }
+        return this.clearSlashes(fragment);
+    }
+
+    navigate = (path = '') => {
+        if (this.mode === 'history') {
+            window.history.pushState(null, null, this.root + this.clearSlashes(path));
+        } else {
+            window.location.href = `${window.location.href.replace(/#(.*)$/, '')}#${path}`;
+        }
+        return this;
+    }
+
+    listen = () => {
+        clearInterval(this.interval);
+        this.interval = setInterval(this.interval, 50);
+    }
+
+    interval = () => {
+        if (this.current === this.getFragment()) return;
+        this.current = this.getFragment();
+        this.routes.some(route => {
+            const match = this.current.match(route.path);
+            if (match) {
+                match.shift();
+                route.cb.apply({}, match);
+                return match;
+            }
+            return false;
+        })
+    }
+
+}
+
 const htmlBuilder = new HtmlBuilder();
 htmlBuilder.buildHeader(0);
+
+const router = new Router({
+    mode: 'hash',
+    root: '/'
+});
+
+router
+    .add(/about/, () => {
+        alert('welcome in about page');
+    })
+    .add(/products\/(.*)\/specification\/(.*)/, (id, specification) => {
+        alert(`products: ${id} specification: ${specification}`);
+    })
+    .add('', () => {
+        console.log('welcome in catch all controller');
+    });
